3f3adcfb76bf0db9c8d7b583d6f0962a7be808bd
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-22 19:50:59 +0200

    Initially added React, Babel and Webpack

---
+++ chat/webpack.config.js
@@ -0,0 +1,19 @@
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+    entry: './app/index.js',
+    output: { path: __dirname, filename: 'public/bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react']
+                }
+            }
+        ]
+    },
+};
