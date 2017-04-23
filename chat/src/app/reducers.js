966de1a0ff61a157a723545aeee88ee26a226433
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2017-04-23 16:57:57 +0200

    Add reducer for screen changing

--- chat/src/app/reducers.js
+++ chat/src/app/reducers.js
@@ -1,10 +1,16 @@
-import { Screens } from "./actions";
+import { Screens, SET_SCREEN } from "./actions";
 
 const initialState = {
     currentScreen: Screens.LOGIN,
 };
 
 function mainApp(state = initialState, action) {
+    switch (action.type) {
+    case SET_SCREEN:
+        return Object.assign({}, state, {
+            currentScreen: action.screen
+        });
+    }
     return state;
 }
 
