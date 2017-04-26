fd6131465bef63fc6a0945da6e7a9884f1c02d97
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-26 13:37:40 +0200

    Remove Redux example component - Counter

--- chat/src/app/reducers/index.js
+++ chat/src/app/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
 import { screen } from "./screen";
 import { websocket } from "./websocket";
-import { counters } from "./counters";
 
 
 export default combineReducers({
     screen,
-    websocket,
-    counters
+    websocket
 });
