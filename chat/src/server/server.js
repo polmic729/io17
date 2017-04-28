1cce741f8dea7aa69087194362ef113b96f2ba00
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-04-29 00:31:18 +0200

    Webpack hot reloading

--- chat/src/server/server.js
+++ chat/src/server/server.js
@@ -5,25 +5,30 @@ let mongoose = require("mongoose");
 let path = require("path");
 let session = require("express-session");
 let mongoStore = require("connect-mongo")(session);
+let webpack = require('webpack')
+let webpackDevMiddleware = require('webpack-dev-middleware')
+let webpackHotMiddleware = require('webpack-hot-middleware')
 
-let config = require("../../config");
 let auth = require("./auth");
+let config = require("../../config");
 let websockets = require("./websockets");
+let webpackConfig = require('../../webpack.config')
 
 const PATH_STYLES = path.resolve(__dirname, "../client/styles");
-const PATH_DIST = path.resolve(__dirname, "../../dist");
 
-// Connect to mongoDB
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongodb.url);
 
 let app = express();
 
+var compiler = webpack(webpackConfig)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
+
 // initialize web sockets
 new websockets(app);
 
 app.use("/styles", express.static(PATH_STYLES));
-app.use(express.static(PATH_DIST));
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client/index.html"));
@@ -40,11 +45,8 @@ app.use((req, res, next) => {
 
 // Error handler
 app.use((err, req, res) => {
-    // Set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // Render the error page
+    res.locals.error = req.app.get("env") === "devel" ? err : {};
     res.status(err.status || 500);
     res.send(err.message);
 });
