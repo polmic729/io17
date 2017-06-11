5728e2c30f6545b1d6594a91f7556c9f76e526f5
Godul <jg370815@students.mimuw.edu.pl>
2017-06-11 21:26:11 +0200

    Websocket functions skeletons

--- chat/src/server/websockets.js
+++ chat/src/server/websockets.js
@@ -1,6 +1,8 @@
 let socketio = require("socket.io");
 let http = require("http");
 let config = require("../../config");
+// let User = require("./models/user");
+// let Room = require("./models/room");
 
 class WebSockets {
 
@@ -14,14 +16,46 @@ class WebSockets {
     }
 
     initializeMessages(io) {
-        // TODO: sending messages only to specified rooms
         io.on("connection", function(socket) {
             socket.on("chat-message", function(message) {
                 io.emit("chat-message", message);
             });
         });
     }
-    // TODO: create room, add user to room, get users of room
+
+    initialize(io) {
+        io.on("connection", function(socket) {
+            socket.on("chat-message", function(message) {
+                socket.join(message.room);
+                io.to(message.room).emit("chat-message", message.message);
+            });
+
+            socket.on("create-room", function(message) {
+                // TODO
+                // createRoom(message.name);
+                // socket.emit("create-room", success/failure);
+            });
+
+            socket.on("add-user", function(message) {
+                // TODO
+                // addUser(message.username);
+                // socket.emit("add-user", success/failure);
+            });
+
+            socket.on("get-users", function(message) {
+                // TODO
+                // let roomUsers = getUsers(message.room_id);
+                // socket.emit("get-users", roomUsers);
+            });
+
+            socket.on("get-rooms", function(message) {
+                // TODO
+                // let userRooms = getRooms(message.username);
+                // socket.emit("get-rooms", userRooms);
+            });
+        });
+    }
+
 }
 
 module.exports = WebSockets;
