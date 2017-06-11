dca998c17d03595a0f163fb3e62df339fc5cceb6
Michał Izworski <m.izworski7@gmail.com>
2017-06-11 16:54:05 +0200

    Group can be selected from menu.

--- chat/src/app/components/chat/Groups.js
+++ chat/src/app/components/chat/Groups.js
@@ -5,18 +5,38 @@ class Groups extends React.Component {
 
     constructor(props) {
         super(props);
+        let selectedGroup = window.sessionStorage.getItem("selectedGroup");
+        if (selectedGroup) {
+            selectedGroup = Number(selectedGroup);
+        } else {
+            selectedGroup = 2137;//todo selected_channel = general_channel
+        }
+        let groups = [2137, 1488, 21, 37];//todo groups = getUserGroups(this.state.username)
+        this.state = {
+            selected_channel: selectedGroup,
+            groups: groups
+        };
+        this.changeGroup = this.changeGroup.bind(this);
     }
 
+    changeGroup(key) {
+        this.setState({
+            selected_channel: key
+        });
+        window.sessionStorage.setItem("selectedGroup", key);
+    }
 
     render() {
+        const groupList = this.state.groups.map((group) =>
+            <div className={ group === this.state.selected_channel ? "groupSelected" : "groupDefault"}
+                 onClick={() => this.changeGroup(group)} key={group}> test_online{group}</div>
+        );
+
         return (
             <div id="chatGroups">
                 <div className="entityContainer">
                     <h3>grupowe</h3>
-                    <div>test_online1</div>
-                    <div>test_online2</div>
-                    <div>test_offline1</div>
-                    <div>test_offline2</div>
+                    { groupList }
                 </div>
             </div>
         );
